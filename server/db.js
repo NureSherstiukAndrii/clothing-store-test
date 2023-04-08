7049832183e35f75f4dde5b5299f36e5bfc31016
Andrey Sherstiuk <sherstukandrey15@gmail.com>
2023-04-08 14:40:27 +0300

    add some back(Шерстюк Андрій)

---
+++ server/db.js
@@ -0,0 +1,87 @@
+const sql = require('mssql');
+let instance = null;
+
+const config = {
+    user: "dsrdsr7",
+    password: "NAMS9kUgcwgQS@S",
+    server: "dsrdsr7v2.database.windows.net",
+    database: "Internet_ cloth_shop_v2",
+    options: {
+        encrypt: true,
+        trustServerCertificate: false
+    },
+};
+
+class DbService {
+    static getDbServiceInstance() {
+        return instance ? instance : new DbService();
+    }
+
+    async getAllTours() {
+        try {
+            const pool = new sql.ConnectionPool(config);
+            const response = await new Promise((resolve, reject) => {
+                pool.connect().then(() => {
+                    const request = new sql.Request(pool);
+                    request.query("SELECT * FROM Products").then((result, err) => {
+                        if(err){
+                            reject(new Error(err.message));
+                        }
+                        resolve(result.recordset);
+                        pool.close();
+                    }).catch((err) => {
+                        console.error(err);
+                        pool.close();
+                    });
+                })
+            })
+            return response
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+
+    async addTour(id, name) {
+        try {
+            await sql.connect(config);
+            const result = await sql.query`INSERT INTO MyTable (Id, Name) VALUES (${id}, ${name})`;
+        } catch (error) {
+            console.log(error);
+        } finally {
+            sql.close();
+        }
+    }
+
+    async deleteTour(id){
+        sql.connect(config)
+            .then(pool => {
+                const request = pool.request();
+                request.query(`DELETE FROM MyTable WHERE id = ${id}`)
+                    .then(result => {
+                        console.log(`Deleted ${result.rowsAffected} rows`);
+                    })
+                    .catch(error => {
+                        console.log(`Error: ${error}`);
+                    });
+            })
+            .catch(error => {
+                console.log(`Error connecting to the database: ${error}`);
+            });
+    }
+
+    async editTour(id, name) {
+        try {
+            await sql.connect(config);
+            const result = await sql.query`UPDATE MyTable SET Name=${name} WHERE Id=${id}`;
+        } catch (error) {
+            console.log(error);
+        } finally {
+            sql.close();
+        }
+    }
+}
+
+module.exports = DbService;
+
+
---
+++ server/index.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const app = express();
+const path = require('path')
+
+const dbService = require("./db");
+
+
+const parentDir = path.resolve(__dirname, '..');
+app.use(express.static(path.join(parentDir)))
+app.use(express.json());
+
+
+app.get("/", function (req, res){
+    res.sendFile( parentDir + "/client/HomePage/index.html");
+})
+
+app.get("/AllTours", function (req, res){
+    res.sendFile(__dirname + "/toursPage.html");
+})
+
+app.get('/script.js', (req, res) => {
+    res.type('text/javascript');
+    res.sendFile(__dirname + '/script.js');
+});
+
+app.get("/getAllTours", function(request, response){
+    const db = dbService.getDbServiceInstance();
+    let result = db.getAllTours();
+
+    result
+        .then((data) => response.json({ data: data }))
+        .catch((err) => console.log(err));
+});
+
+
+app.post('/insertTour', (req, res) => {
+    const {id, name} = req.body;
+    const db = dbService.getDbServiceInstance()
+
+    const result = db.addTour(+id, name)
+
+    result
+        .then((data) => {res.json({ data: data }); console.log(data)})
+        .catch((err) => console.log(err));
+});
+
+app.delete("/deleteTour/:id", (request, response) => {
+    const { id } = request.params;
+    const db = dbService.getDbServiceInstance();
+
+    const result = db.deleteTour(id);
+
+    result
+        .then((data) => response.json({ success: data }))
+        .catch((err) => console.log(err));
+});
+
+app.patch("/updateTour", (request, response) => {
+    console.log(request);
+    const {id, name} = request.body;
+    const db = dbService.getDbServiceInstance();
+    const result = db.editTour(+id, name);
+    result
+        .then((data) => response.json({ success: data }))
+        .catch((err) => console.log(err));
+});
+
+app.listen(3000);
